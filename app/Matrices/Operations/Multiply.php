a8b3f171f70e6b7ce92a76d9c1fde82218cccebf
korashy <a.ashraf.elgammal@gmail.com>
2020-12-09 04:30:32 +0200

    implement the multiplication of two matrices logic

--- app/Matrices/Operations/Multiply.php
+++ app/Matrices/Operations/Multiply.php
@@ -7,4 +7,20 @@ namespace App\Matrices\Operations;
 class Multiply
 {
 
+    public static function handle(array $first, array $second) :array
+    {
+        $firstRows = count($first);
+        $firstCols = count($first[0]);
+        $secondCols = count($second[0]);
+        $result = [] ;
+        for ($i=0; $i < $firstRows; $i++) {
+            for($j=0; $j < $secondCols; $j++){
+                $result[$i][$j] = 0;
+                for($k=0; $k < $firstCols; $k++)
+                    $result[$i][$j] += $first[$i][$k] * $second[$k][$j];
+            }
+        }
+    return $result;
+    }
+
 }
