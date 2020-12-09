f7b35405d2a4d3e6f899578a85c830333adf1b60
korashy <a.ashraf.elgammal@gmail.com>
2020-12-09 04:29:52 +0200

    implement excel columns alike number converter

--- app/Matrices/NumberTOExcelColumnsConverter.php
+++ app/Matrices/NumberTOExcelColumnsConverter.php
@@ -6,5 +6,14 @@ namespace App\Matrices;
 
 class NumberTOExcelColumnsConverter
 {
-
+    public static function convert( int $number): string
+    {
+        $columnName = "";
+        while ($number > 0) {
+            $modulo = ($number - 1) % 26;
+            $columnName = chr(65 + $modulo) . $columnName;
+            $number = (int)(($number - $modulo) / 26);
+        }
+        return $columnName;
+    }
 }
